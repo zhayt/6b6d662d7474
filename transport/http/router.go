0982cc230aadf803db27f1389f92006efd3e11d4
Aybek <turk.883ar@gmail.com>
2023-05-12 07:58:47 -0700

    fix issue in endpoints url

--- transport/http/router.go
+++ transport/http/router.go
@@ -8,8 +8,8 @@ import (
 func (s *Server) InitRoute() http.Handler {
 	r := mux.NewRouter()
 
-	r.HandleFunc("api/v1/currency/save/{date}", s.handler.SaveCurrency).Methods("GET")
-	r.HandleFunc("api/v1/currency/{date}/{code}", s.handler.ShowCurrency).Methods("GET")
+	r.HandleFunc("/api/v1/currency/save/{date}", s.handler.SaveCurrency).Methods("GET")
+	r.HandleFunc("/api/v1/currency/{date}/{code}", s.handler.ShowCurrency).Methods("GET")
 
 	return r
 }
