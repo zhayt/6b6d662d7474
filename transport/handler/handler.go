f205f67ebee326b469cc718126c261ef13d32874
Aybek <turk.883ar@gmail.com>
2023-05-12 10:29:47 -0700

    update handlers

--- transport/handler/handler.go
+++ transport/handler/handler.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
+	"context"
+	"errors"
 	"github.com/gorilla/mux"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/zhayt/kmf-tt/service"
 	"go.uber.org/zap"
 	"net/http"
@@ -10,6 +13,14 @@ import (
 type Handler struct {
 	currency *service.CurrencyService
 	l        *zap.Logger
+}
+
+type SuccessResponse struct {
+	Success bool `json:"success"`
+}
+
+type ErrorResponse struct {
+	Message string `json:"message"`
 }
 
 func NewHandler(currency *service.CurrencyService, l *zap.Logger) *Handler {
@@ -23,6 +34,8 @@ func (h *Handler) SaveCurrency(w http.ResponseWriter, r *http.Request) {
 
 	if err := h.currency.SaveCurrency(context.TODO(), date); err != nil {
 
+		h.l.Error("SaveCurrency error", zap.Error(err))
+
 		if errors.Is(err, service.ErrUserStupid) {
 			h.respondWithError(w, http.StatusBadRequest, "The entered date is not valid.")
 			return
@@ -32,60 +45,65 @@ func (h *Handler) SaveCurrency(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	h.l.Info("Currency saved", zap.String("date", date))
+
 	h.respondWithSuccess(w, http.StatusOK)
 }
 
 func (h *Handler) ShowCurrency(w http.ResponseWriter, r *http.Request) {
+	date := mux.Vars(r)["date"]
+	code := mux.Vars(r)["code"]
 
+	h.l.Info("Get url param", zap.String("date", date), zap.String("code", code))
+
+	// rest of the handler code
+	currencies, err := h.currency.GetCurrency(context.TODO(), date, code)
+	if err != nil {
+		h.l.Error("GetCurrency error", zap.Error(err))
+		if errors.Is(err, service.ErrUserStupid) {
+			h.respondWithError(w, http.StatusBadRequest, "The entered date is not valid.")
+			return
+		}
+
+		h.respondWithError(w, http.StatusInternalServerError, "Couldn't get currencies")
+		return
+	}
+
+	if len(currencies) == 0 {
+		h.l.Error("Zero currencies error", zap.Error(err))
+		h.respondWithError(w, http.StatusNotFound, http.StatusText(http.StatusNotFound))
+		return
+	}
+
+	h.l.Info("Currencies founded", zap.Int("amount", len(currencies)))
+
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
+
+	json.NewEncoder(w).Encode(currencies)
 }
 
 func (h *Handler) respondWithError(w http.ResponseWriter, code int, message string) {
-	h.l.Error(message)
-
-	response := model.ErrorResponse{
+	response := ErrorResponse{
 		Message: message,
 	}
 
-	json, err := formatToJSON(response)
-	if err != nil {
-		h.l.Error("formatToJSON error", zap.Error(err))
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("something wrong try later!"))
-		return
-	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	w.Write(json)
+
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
+
+	json.NewEncoder(w).Encode(response)
 }
 
 func (h *Handler) respondWithSuccess(w http.ResponseWriter, code int) {
-	response := model.SuccessResponse{
+	response := SuccessResponse{
 		Success: true,
-	}
-
-	json, err := formatToJSON(response)
-	if err != nil {
-		h.l.Error("formatToJSON error", zap.Error(err))
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("something wrong try later!"))
-		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	w.Write(json)
-}
 
-func formatToJSON(input interface{}) ([]byte, error) {
-	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
 
-	switch v := input.(type) {
-	case model.SuccessResponse:
-		return json.Marshal(input.(model.SuccessResponse))
-	case model.ErrorResponse:
-		return json.Marshal(input.(model.ErrorResponse))
-	default:
-		return nil, fmt.Errorf("unsupported type: %T", v)
-	}
+	json.NewEncoder(w).Encode(response)
 }
